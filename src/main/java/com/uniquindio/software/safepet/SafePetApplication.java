4b00f39db567df5e241ffd7dd9bce9747c5aacc2
Santiago <santiago.velandiag@uqvirtual.edu.co>
2021-10-25 10:50:37 -0500

    Se crea el proyecto

---
+++ src/main/java/com/uniquindio/software/safepet/SafePetApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.software.safepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafePetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniquindio/software/safepet/SafePetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.software.safepet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafePetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
